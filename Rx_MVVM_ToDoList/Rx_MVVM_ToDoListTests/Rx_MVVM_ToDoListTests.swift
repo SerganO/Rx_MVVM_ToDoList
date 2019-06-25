bb6a5478571042558e6b6f18f17c715522412fbb
Serhii Ostrovetskyi <so@rollncode.com>
2019-06-25 16:25:55 +0300

    init

---
+++ Rx_MVVM_ToDoList/Rx_MVVM_ToDoListTests/Rx_MVVM_ToDoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rx_MVVM_ToDoListTests.swift
+//  Rx_MVVM_ToDoListTests
+//
+//  Created by Trainee on 6/25/19.
+//  Copyright © 2019 Trainee. All rights reserved.
+//
+
+import XCTest
+@testable import Rx_MVVM_ToDoList
+
+class Rx_MVVM_ToDoListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
